d42c421a4a6dc15f51ff43cfce6f2eb1e61753c1
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-11-09 01:54:32 +0200

    initial Commit

---
+++ FindAVenue/src/main/java/com/findavenue/FindAVenue/FindAVenueApplication.java
@@ -0,0 +1,13 @@
+package com.findavenue.FindAVenue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAVenueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAVenueApplication.class, args);
+	}
+
+}
---
+++ FindAVenue/src/test/java/com/findavenue/FindAVenue/FindAVenueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findavenue.FindAVenue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindAVenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
